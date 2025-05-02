a4b7cf31f5322d7e4cf2864017ac59226677e9df
Natalia-Pixell <natalia.jagiela@stud-provadis-hochschule.de>
2025-05-02 08:56:10 +0000

    anpassungen an index

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
+      fontSize: {
+        '62xl': ['4.0rem', { lineHeight: '1' }],
+        '42xl': ['2.5rem', { lineHeight: '1' }]
+      },
       fontFamily: {
         poppins: ['Poppins', 'sans'],
         tt: ['Tiemes New Roman', 'serif'],
